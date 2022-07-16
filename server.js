227b05855e8945022af6574db8309e78c1dec6e2
Justice Kelechi <smartcode.kc@gmail.com>
2022-07-16 13:47:56 -0700

    first commit

---
+++ server.js
@@ -0,0 +1,28 @@
+require("dotenv").config({ path: "./config.env" });
+const express = require("express");
+const connectDB = require("./config/db");
+const errorHandler = require("./middleware/error");
+
+//connect DB
+connectDB();
+
+const app = express();
+
+app.use(express.json());
+
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/private", require("./routes/private"));
+
+//Error Handler (Should be last pice of middleware)
+app.use(errorHandler);
+
+const PORT = process.env.PORT || 5000;
+
+const server = app.listen(PORT, () =>
+  console.log(`server running on port ${PORT}`)
+);
+
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`Logged Error: ${err.message}`);
+  server.close(() => process.exit(1));
+});
